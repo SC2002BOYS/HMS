8535bcc28e48dd416ea52a167e7b333c3fc9dde7
shawnchow7 <132461800+shawnchow7@users.noreply.github.com>
2024-11-15 23:06:10 +0800

    add XY doc class and admin and its controller class

---
+++ src/Controller/InventoryController.java
@@ -0,0 +1,9 @@
+package Controller;
+
+public class InventoryController {
+    // Method to update medication stock level
+    // need to chnage logic, this is just a placeholder
+    public void updateMedicationStockLevel(String medicationId, int newStockLevel) {
+        System.out.println("Updating medication stock level...");
+    }
+}
---
+++ src/Model/Doctor.java
@@ -0,0 +1,42 @@
+package Model;
+
+import java.util.ArrayList;
+import View.DoctorMenu;
+import Type.Role;
+
+public class Doctor extends User{
+
+
+    //attributes
+    private ArrayList<MedicalRecord> medicalRecords;
+    
+    //private ArrayList<AppointmentRequest> appointmentRequests;
+    //[comment out waiting for XY]private Schedule schedule;
+
+    private ArrayList<Patient> patients;
+    private final DoctorMenu menu = new DoctorMenu();
+
+    public Doctor(String userID,String userPass, Role role, ArrayList<MedicalRecord> medicalRecords){
+        super(userID, userPass, role);
+        //this.medicalRecords = medicalRecords;
+
+    }
+
+    //To print the doctor menu
+    public void printMenu(){
+        this.menu.printMenu();
+    }
+
+    //Get medical records
+    public ArrayList<MedicalRecord> getMedicalRecords() { return this.medicalRecords; }
+
+    //Get appointment requests
+    //public ArrayList<AppointmentRequest> getAppointmentRequests(){ return this.appointmentRequests; }
+
+    //Get schedule
+    //[comment out waiting for XY]public Schedule getSchedule(){ return this.schedule; }
+
+    //Get patients
+    //public ArrayList<Patient> getPatients(){ return this.patients; }
+}
+
